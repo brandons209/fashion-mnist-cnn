abcadc73094d2beb88ce135ce2cd20de96d7c315
Brandon Silva <brandons209@aol.com>
2018-09-28 14:50:49 -0400

    finished model, basic functions of program, still some todos

--- fashion-mnist.py
+++ fashion-mnist.py
@@ -1,7 +1,7 @@
 #general imports
 import numpy as np
 import time
-import pickle
+import pandas
 
 #data set imports
 from keras.datasets import fashion_mnist
@@ -20,9 +20,68 @@ from keras import optimizers as opt
 train_labels, test_labels = to_categorical(train_labels, num_classes=10), to_categorical(test_labels, num_classes=10)
 valid_split = 0.1
 
+## TODO: Augment data
+
 print("Dataset Stats")
 print("Training images shape: {}".format(train_images.shape))
 print("There are {} training images, {} testing images, and the validation split is {}% of training data".format(len(train_images), len(test_images), valid_split*100))
-
+print(train_images.shape[1:])
 #build model
 cnn = Sequential()
+
+cnn.add(Conv2D(filters=256, kernel_size=2, padding='same', activation='relu', input_shape=train_images.shape[1:]))
+cnn.add(Conv2D(filters=256, kernel_size=2, padding='same', activation='relu'))
+cnn.add(Conv2D(filters=256, kernel_size=2, padding='same', activation='relu'))
+cnn.add(Conv2D(filters=256, kernel_size=2, padding='same', activation='relu'))
+cnn.add(MaxPooling2D(strides=2, padding='same'))
+cnn.add(Dropout(0.3))
+
+cnn.add(Conv2D(filters=128, kernel_size=2, padding='same', activation='relu'))
+cnn.add(Conv2D(filters=128, kernel_size=2, padding='same', activation='relu'))
+cnn.add(Conv2D(filters=128, kernel_size=2, padding='same', activation='relu'))
+cnn.add(Conv2D(filters=128, kernel_size=2, padding='same', activation='relu'))
+cnn.add(MaxPooling2D(strides=2, padding='same'))
+cnn.add(Dropout(0.4))
+
+cnn.add(Conv2D(filters=64, kernel_size=2, padding='same', activation='relu'))
+cnn.add(Conv2D(filters=64, kernel_size=2, padding='same', activation='relu'))
+cnn.add(Conv2D(filters=64, kernel_size=2, padding='same', activation='relu'))
+cnn.add(Conv2D(filters=64, kernel_size=2, padding='same', activation='relu'))
+cnn.add(Dropout(0.4))
+
+cnn.add(Flatten())
+cnn.add(Dense(300, activation='relu'))
+cnn.add(Dropout(0.3))
+cnn.add(Dense(100, activation='relu'))
+cnn.add(Dense(10, activation='softmax'))
+
+cnn.summary()
+input("Press enter to start training model...")
+#hyperparameters
+epochs = 2
+batch_size = 64
+learn_rate = 0.01
+
+cnn.compile(loss='categorical_crossentropy', optimizer=opt.RMSProp(lr=learn_rate), metrics=['accuracy', 'top_k_categorical_accuracy'])
+
+weight_save_path = 'fashion.mnist.best.weights.hdf5'
+checkpointer = ModelCheckpoint(filepath=weight_save_path, verbose=1, save_best_only=True)
+
+start_time = time.strftime("%a_%b_%d_%Y_%H:%M", time.localtime())
+ten_board = TensorBoard(log_dir='tensorboard_logs/{}_fashion_mnist'.format(start_time), write_images=True)
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=2, verbose=1, min_lr=1*10**(-11))
+#TODO add early stop callback if needed
+
+#train model
+history = cnn.fit(train_images, train_labels, batch_size=batch_size, epochs=epochs, callbacks=[checkpointer, ten_board, reduce_lr], validation_split=valid_split)
+
+#test model
+cnn.load_weights(weight_save_path)
+testing_results = cnn.evaluate(test_images, test_labels)
+print("Testing Loss: {:.4f}, Testing Accuracy: {:.2f}%, Top 5 Accuracy: {:.2f}%.\n".format(testing_results[0], testing_results[1], testing_results[2]))
+
+model_save_path = "saved_models/"+start_time+"_fashion_mnist.h5"
+print("Saving model to {}".format(model_save_path))
+cnn.save(model_save_path)
+
+##TODO: save history to csv file with pandas
