e5441237cdf45286a01b8488441aea03b84df938
Brandon Silva <brandons209@aol.com>
2018-09-30 15:49:50 -0400

    finished model, readme. started on graphing data

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# fashion-mnist-cnn
-CNN using Fashion-MNIST data set
+# Fashion MNIST CNN
+## CNN trained on the Fashion MNIST data set
+***
+The Fashion MNIST data set contains 60,000 training images and 10,000 testing images of 10 different kinds of clothing. It is like the MNIST data set, with pictures 28x28 pixels and 10 classes. This data set is becoming the new standard for testing and teaching people about neural networks.
+[LICENSE](LICENSE)
+***
+### Libraries
+For this project, I used the Keras deep learning library with the TensorFlow backend. I also used matplotlib for graphing history from training models. TensorBoard is also used to monitor loss and accuracy during training.
+### Model Architecture
+This model employs two convolutional layers, followed by a max pooling layer. These layers are connected to a fully connected dense layer of 400 nodes, then to the output layer of 10 nodes, each for the different class of clothing. Dropout is also employed after the max pooling layers and the fully connected hidden layer to help with overfitting. I started with a learning rate of 0.001 and trained for 20 epochs.
+### Running the Model
+1. Install required libraries in requirements.txt file.
+```bash
+pip install -r requirements.txt
+```
+2. Change hyper-parameters and model structure as needed.
+```python
+#hyperparameters
+epochs = 20
+batch_size = 64
+learn_rate = 0.001
+```
+3. (Optional) Start TensorBoard to view loss and accuracy graphs as model trains.
+```bash
+tensorboard --logdir=tensorboard_logs
+```
+4. Run the model.
+```bash
+python fashion-mnist.py
+```
+5. View history graphs with graph_history file.
+```bash
+python graph_history /path/to/history/file.pkl
+```
+### Performance
+With a decent size network for this data set, I was able to score 93% validation accuracy with 100% top 5 accuracy. This data set is small and relatively simple, so it can be easy to over fit. I was trying to get the highest accuracy possible, so I am going to try to fine tune the model some more. It seems that the model converges around 0.222 validation loss, even with various tweaks and slightly different models. A better model size, perhaps with batch normalization and some other techniques, can get a higher accuracy.
--- fashion-mnist.py
+++ fashion-mnist.py
@@ -12,7 +12,7 @@ from keras.utils import to_categorical
 
 #model imports
 from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Dropout, Flatten, Dense
+from keras.layers import Dropout, Flatten, Dense, BatchNormalization
 from keras.models import Sequential
 
 #training
@@ -24,66 +24,62 @@ from keras import optimizers as opt
 train_labels, test_labels = to_categorical(train_labels, num_classes=10), to_categorical(test_labels, num_classes=10)
 valid_split = 0.1
 
-#augment data, validation split gives subset parameter for flow function
-#augmented_data_gen = ImageDataGenerator(rotation_range=270, width_shift_range=0.2, height_shift_range=0.2, horizontal_flip=True, vertical_flip=True, validation_split=valid_split)
-
 #normalize pixel values from [0, 255] to [0, 1]
 train_images = train_images.astype('float32') / 255
 test_images = test_images.astype('float32') / 255
 
+#reshape images to (num_samples, 28, 28, 1), since images are one channel.
+train_images = np.expand_dims(train_images, 3)
+test_images = np.expand_dims(test_images, 3)
+
+#augment data, validation split gives subset parameter for flow function
+#augmented_data_gen = ImageDataGenerator(rotation_range=270, width_shift_range=0.2, height_shift_range=0.2, horizontal_flip=True, vertical_flip=True, validation_split=valid_split)
+#augmented_data_gen.fit(train_images)
+
 print("Dataset Stats")
 print("Training images shape: {}".format(train_images.shape))
-print("There are {} training images, {} testing images, and the validation split is {}% of training data, which is {} images.".format(len(train_images), len(test_images), valid_split*100, len(train_images)*valid_split))
+print("There are {} training images, {} testing images, and the validation split is {}% of training data, which is {:.0f} images.".format(len(train_images), len(test_images), valid_split*100, len(train_images)*valid_split))
 
 #build model
 cnn = Sequential()
 
+#cnn.add(BatchNormalization(input_shape=train_images.shape[1:]))
 cnn.add(Conv2D(filters=256, kernel_size=2, padding='same', activation='relu', input_shape=train_images.shape[1:]))
-cnn.add(Conv2D(filters=256, kernel_size=2, padding='same', activation='relu'))
-cnn.add(Conv2D(filters=256, kernel_size=2, padding='same', activation='relu'))
-cnn.add(Conv2D(filters=256, kernel_size=2, padding='same', activation='relu'))
 cnn.add(MaxPooling2D(strides=2, padding='same'))
 cnn.add(Dropout(0.3))
 
-cnn.add(Conv2D(filters=128, kernel_size=2, padding='same', activation='relu'))
-cnn.add(Conv2D(filters=128, kernel_size=2, padding='same', activation='relu'))
-cnn.add(Conv2D(filters=128, kernel_size=2, padding='same', activation='relu'))
-cnn.add(Conv2D(filters=128, kernel_size=2, padding='same', activation='relu'))
+cnn.add(Conv2D(filters=150, kernel_size=2, padding='same', activation='relu'))
 cnn.add(MaxPooling2D(strides=2, padding='same'))
-cnn.add(Dropout(0.4))
-
-cnn.add(Conv2D(filters=64, kernel_size=2, padding='same', activation='relu'))
-cnn.add(Conv2D(filters=64, kernel_size=2, padding='same', activation='relu'))
-cnn.add(Conv2D(filters=64, kernel_size=2, padding='same', activation='relu'))
-cnn.add(Conv2D(filters=64, kernel_size=2, padding='same', activation='relu'))
 cnn.add(Dropout(0.4))
 
 cnn.add(Flatten())
-cnn.add(Dense(300, activation='relu'))
+cnn.add(Dense(400, activation='relu'))
 cnn.add(Dropout(0.3))
-cnn.add(Dense(100, activation='relu'))
 cnn.add(Dense(10, activation='softmax'))
 
 cnn.summary()
 input("Press enter to start training model...")
 #hyperparameters
-epochs = 2
+epochs = 20
 batch_size = 64
-learn_rate = 0.01
+learn_rate = 0.001 #if starting lr is 0.01, model doesnt train. Kinda interesting...
 
-cnn.compile(loss='categorical_crossentropy', optimizer=opt.RMSProp(lr=learn_rate), metrics=['accuracy', 'top_k_categorical_accuracy'])
+cnn.compile(loss='categorical_crossentropy', optimizer=opt.RMSprop(lr=learn_rate), metrics=['accuracy', 'top_k_categorical_accuracy'])
 
 weight_save_path = 'fashion.mnist.best.weights.hdf5'
 checkpointer = ModelCheckpoint(filepath=weight_save_path, verbose=1, save_best_only=True)
 
 start_time = time.strftime("%a_%b_%d_%Y_%H:%M", time.localtime())
 ten_board = TensorBoard(log_dir='tensorboard_logs/{}_fashion_mnist'.format(start_time), write_images=True)
-reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=2, verbose=1, min_lr=1*10**(-11))
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=1, verbose=1, min_lr=1*10**(-8))
 #TODO add early stop callback if needed
 
 #train model
 time_to_train = time.time()
-#history = cnn.fit_generator(augmented_data_gen.flow(train_images, train_labels, batch_size=batch_size, subset='training'), steps_per_epoch=train_images.shape[0] // batch_size, epochs=epochs, verbose=1, callbacks=[checkpointer, ten_board, reduce_lr], validation_data=augmented_data_gen.flow(train_images, train_labels, batch_size=batch_size, subset='validation'), validation_steps=(train_images.shape[0]*valid_split) // batch_size, use_multiprocessing=True)
+#history = cnn.fit_generator(augmented_data_gen.flow(train_images, train_labels, batch_size=batch_size, subset='training'),
+#steps_per_epoch=train_images.shape[0] // batch_size, epochs=epochs, verbose=1, callbacks=[checkpointer, ten_board, reduce_lr],
+#validation_data=augmented_data_gen.flow(train_images, train_labels, batch_size=batch_size, subset='validation'),
+#validation_steps=(train_images.shape[0]*valid_split) // batch_size, use_multiprocessing=True)
 history = cnn.fit(train_images, train_labels, batch_size=batch_size, epochs=epochs, callbacks=[checkpointer, ten_board, reduce_lr], validation_split=valid_split)
 time_to_train = time.time() - time_to_train
 print("Model took {:.0f} minutes to train".format(time_to_train/60))
@@ -98,7 +94,7 @@ model_save_path = "saved_models/"+start_time+"_fashion_mnist.h5"
 print("Saving model to {}".format(model_save_path))
 cnn.save(model_save_path)
 
-history_save_path = "{}_cnn_history.pkl".format(start_time)
+history_save_path = "saved_histories/{}_cnn_history.pkl".format(start_time)
 print("Saving history to {}.".format(history_save_path))
 with open(history_save_path, 'wb') as f:
     pickle.dump(history.history, f)
